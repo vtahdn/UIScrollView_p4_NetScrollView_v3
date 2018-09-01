aa2c2d1d42b1e5ed83abd55308536a481f584708
Viet Doan <vtahdn@icloud.com>
2018-09-01 12:05:54 +0700

    Initial Commit

---
+++ UIScrollView_p4_NetScrollView_v3Tests/UIScrollView_p4_NetScrollView_v3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UIScrollView_p4_NetScrollView_v3Tests.swift
+//  UIScrollView_p4_NetScrollView_v3Tests
+//
+//  Created by macbook pro  on 9/1/18.
+//  Copyright © 2018 Viet. All rights reserved.
+//
+
+import XCTest
+@testable import UIScrollView_p4_NetScrollView_v3
+
+class UIScrollView_p4_NetScrollView_v3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
